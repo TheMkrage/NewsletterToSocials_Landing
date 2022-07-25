89932d769226baef6f6868ed657eca53faab4da9
Matthew Krager <matthewkrager@gmail.com>
2022-07-24 18:03:00 -0700

    new google

--- src/analytics/index.js
+++ src/analytics/index.js
@@ -1,5 +1,4 @@
-import ReactGA from 'react-ga';
-
+import ReactGA from "react-ga4";
 export const initGA = () => {
   console.log('GA init');
   ReactGA.initialize(process.env.GOOGLE_ANALYTICS_TRACKING_ID);
@@ -8,7 +7,7 @@ export const initGA = () => {
 export const logPageView = () => {
   console.log(`Logging pageview for ${window.location.pathname}`);
   ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
+  ReactGA.send(window.location.pathname);
 };
 
 export const logEvent = (category = '', action = '') => {
